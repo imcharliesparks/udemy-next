29f5d4c770f9849a0d78c3cae97e94abf8763d6e
Charlie Sparks <sparksfullstack@gmail.com>
2022-06-20 15:36:58 -0700

    pushing with all changes

--- pages/news/index.tsx
+++ pages/news/index.tsx
@@ -1,5 +1,13 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
 
-const News: NextPage = () => <div>news page</div>
+const News: NextPage = () => (
+	<>
+		<h1>news page</h1>
+		<ul>
+			<Link href="/news/123">News Pages</Link>
+		</ul>
+	</>
+)
 
 export default News
